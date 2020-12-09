71c25daf803d31bf78816c42a64760ae64241733
Дмитрий <bushuev.0221@gmail.com>
2020-12-09 19:44:07 +0400

    test v 2.0

---
+++ app/src/main/java/com/example/mail/AddListItem.java
@@ -0,0 +1,14 @@
+package com.example.mail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_list_item);
+    }
+}
