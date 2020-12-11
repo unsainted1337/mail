23493f7c11ba508c54ac8aad99918960bd36d559
Дмитрий <bushuev.0221@gmail.com>
2020-12-11 07:13:41 +0400

    test v 2.0

--- app/src/main/java/com/example/mail/AddListItem.java
+++ app/src/main/java/com/example/mail/AddListItem.java
@@ -2,13 +2,57 @@ package com.example.mail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddListItem extends AppCompatActivity {
+
+    TextInputLayout name;
+    TextInputEditText Lname;
+    TextInputLayout birthday;
+    TextInputLayout adressMail;
+    TextInputEditText inptName;
+    TextInputEditText inptLname;
+    TextInputEditText inptBirthday;
+    TextInputEditText inptMailAdress;
+    TextInputEditText inptMailText;
+    MaterialButton sendMail;
+    MaterialButton save;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_list_item);
+
+        name = findViewById(R.id.namelay);
+        Lname = findViewById(R.id.lnamelay);
+        birthday = findViewById(R.id.birthdaylay);
+        adressMail= findViewById(R.id.adressMaillay);
+        inptName = findViewById(R.id.inputName);
+        inptLname = findViewById(R.id.inputLName);
+        inptBirthday = findViewById(R.id.inputbithday);
+        inptMailAdress = findViewById(R.id.inputMailadress);
+        inptMailText = findViewById(R.id.inputMailText);
+
     }
+
+    public void save_data(View view){
+
+        Intent intent = new Intent(this,MainActivity.class);
+
+        intent.putExtra("name",inptName.getText().toString());
+        intent.putExtra("Lname",inptLname.getText().toString());
+        intent.putExtra("birthday", inptBirthday.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+
+
 }
--- app/src/main/java/com/example/mail/MainActivity.java
+++ app/src/main/java/com/example/mail/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mail;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
 
+    int req_code_name;
+    String name;
+    String Lname;
+    String birthday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<data_get_set> arrayList = new ArrayList<>();
 
         arrayList.add(new data_get_set("name", "lname","09.04.2002",0));
+        arrayList.add(new data_get_set(name, Lname,birthday,0));
 
       ArrAdapter arrAdapter = new ArrAdapter(this, R.layout.list_row,arrayList);
 
@@ -36,7 +43,18 @@ public class MainActivity extends AppCompatActivity {
     public void add_list_item(View view) {
 
         Intent intent = new Intent(this,AddListItem.class);
-        startActivity(intent);
+        startActivityForResult(intent,req_code_name);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+             name = getIntent().getStringExtra("name");
+             Lname = getIntent().getStringExtra("Lname");
+             birthday = getIntent().getStringExtra("birthday");
+
+        }
     }
 }
